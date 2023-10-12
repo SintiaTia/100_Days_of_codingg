c0069bc8d7b4f67c2a8a6fc5d6d976affa7cf457
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-10-12 12:40:11 +0800

    Day4
    membuat kata yang ketika di run akan menampilkan kata yang terulang sebanyak 10 kali

---
+++ Dayfour.java
@@ -0,0 +1,13 @@
+package Days;
+
+public class Dayfour {
+    public static void main(String[] args) {
+   
+        for (int i = 1; i < 10; i++) {
+             System.out.println("Informatics Study Club");
+             
+             System.out.println("Sintia");
+             
+        }
+    }
+}
